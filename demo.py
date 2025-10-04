eb264339139338196715e02959108404ae59fd70
Miikael Volkonski <miikael@volkonski.com>
2025-10-04 21:26:32 +0300

    hedge list

--- demo.py
+++ demo.py
@@ -1,46 +1,12 @@
 def sonic_how_many_rings_did_you_get(input_string):
     
     counter = 0
-    
-    for char in input_string:
-        match char:
-            case 'a':
-                counter += 1
-            case 'b':
-                counter += 1
-            case 'B':
-                counter += 2
-            case 'd':
-                counter += 1
-            case 'D':
-                counter += 1
-            case 'e':
-                counter += 1
-            case 'g':
-                counter += 2
-            case 'o':
-                counter += 1
-            case 'O':
-                counter += 1
-            case 'p':
-                counter += 1
-            case 'P':
-                counter += 1
-            case 'q':
-                counter += 1
-            case 'Q':
-                counter += 1
-            case 'R':
-                counter += 1
-            case '0':
-                counter += 1
-            case '6':
-                counter += 1
-            case '8':
-                counter += 2
-            case '9':
-                counter += 1
- 	
+    for c in input_string:
+        if c in ['a', 'b', 'd', 'D', 'e', 'g', 'o', 'O', 'p', 'P', 'q', 'Q', 'R', '0', '6', '9']:
+            counter += 1
+        elif c in ['B', '8']:
+            counter += 2
+            
     return counter
 
-print(sonic_how_many_ring1s_did_you_get("Helloo"))
+print(sonic_how_many_rings_did_you_get("Helloo"))
