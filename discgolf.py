b07d292b5121828ea75c1e31aa603627f38b2851
Miikael Volkonski <miikael@volkonski.com>
2025-10-23 11:08:46 +0300

    asd

--- discgolf.py
+++ discgolf.py
@@ -1,7 +1,7 @@
 scores = [{
     'bob': 3,
-    'patric': 2,
-    'squidward': 4,
+    'patric': 3,
+    'squidward': 2,
 }, {
     'bob': 3,
     'patric': 3,
@@ -9,7 +9,7 @@ scores = [{
 }, {
     'bob': 3,
     'patric': 3,
-    'squidward': 2,
+    'squidward': 3,
 }, {
     'bob': 3,
     'patric': 3,
@@ -18,22 +18,37 @@ scores = [{
 
 def order_discgolfers(scores):
     result = []
+    players = sorted(scores[0].keys())
     if len(scores) < 1 or scores == None:
         return result
     
-    while len(result) < len(scores[0]):
+    # while len(result) <= len(players):
+    while len(result) <= 2:
         for i in range(len(scores) -1, -1, -1):
-            if len(set(scores[i].values())) == len(scores[i]):
+            print("i is:", i)
+            if len(set(scores[i].values())) > 1:
+                
                 lowest = min(scores[i], key=scores[i].get)
                 if lowest not in result:
+                    print("lowest:", lowest)
                     result.append(lowest)
-                    # print(result)
                     scores[i].pop(lowest)
-                
-                
-            
+            if len(set(scores[i].values())) == 1 and len(scores[i]) > 1:
+                print("tied score:", scores[i])
+                sorted_score = sorted(scores[i].keys())
+                print("sorted score:", sorted_score)
+                if sorted_score[0] not in result:
+                    result.append(sorted_score[0])
+            #         # print(result)
+                    scores[i].pop(sorted_score[0])
+            print("result:", result)
+                    
+        # for player in players:
+        #     if player not in result:
+        #         result.append(player)
+        #         break
         
+    print("scores:", scores)
     return result 
-
 
 print(order_discgolfers(scores))
